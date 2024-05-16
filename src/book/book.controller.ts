74c5b1ffcb791769dcde0ca08b69057b195244db
Muskan3dev <muskannovember3@gmail.com>
2024-05-17 00:10:06 +0530

    Added get controller for getting all books

---
+++ src/book/book.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get, Post, Put, Delete } from '@nestjs/common';
+import { BookService } from './book.service';
+import { Book } from './schemas/book.schema';
+
+@Controller('books')
+export class BookController {
+    constructor(private bookService: BookService) { }
+
+    @Get()
+    async getAllBooks(): Promise<Book[]> {
+        return this.bookService.findAll()
+    }
+
+}
