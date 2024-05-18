ebd8a1fdcf33110fc4a14b113f89e9a9ad230590
Muskan3dev <muskannovember3@gmail.com>
2024-05-18 13:39:22 +0530

    Added post signup route controller

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Body, Post } from '@nestjs/common';
+import { sign } from 'crypto';
+import { AuthService } from './auth.service';
+import { SignUpDto } from './dto/signup.dto'
+
+@Controller('auth')
+export class AuthController {
+    constructor(private authService: AuthService) { }
+    @Post('/signup')
+    signUp(@Body() signUpDto: SignUpDto): Promise<{ token: string }> {
+        return this.authService.signUp(signUpDto);
+    }
+}
