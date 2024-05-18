effc4119f51ef715639b4cd4453c014a94225134
Muskan3dev <muskannovember3@gmail.com>
2024-05-18 21:44:46 +0530

    Added user reference in book schema

--- src/book/schemas/book.schema.ts
+++ src/book/schemas/book.schema.ts
@@ -1,4 +1,6 @@
 import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { User } from "../../auth/schemas/user.schema";
+import mongoose from "mongoose";
 
 export enum Category {
     ADVENTURE = 'Adeventure',
@@ -21,6 +23,9 @@ export class Book {
 
     @Prop()
     category: Category
+
+    @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'User' })
+    user: User
 }
 
 export const BookSchema = SchemaFactory.createForClass(Book)
